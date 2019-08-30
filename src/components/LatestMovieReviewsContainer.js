69bb0137942cc00f4ba79555ea2b8337ec27de69
github username <github email address>
2019-08-30 14:37:56 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -3,7 +3,32 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+// const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+//             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component{
+
+    state = {
+        reviews: []
+}
+
+componentDidMount(){
+    fetch('https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ')
+    .then(res => res.json())
+    .then(data => this.setState({reviews: data.results}))
+}
+render(){
+    return(
+        <div className='latest-movie-reviews'>
+        
+        <MovieReviews reviews={this.state.reviews}/>
+      
+        </div>
+    )
+
+}
+
+}
+
+export default LatestMovieReviewsContainer
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,36 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const NYT_API_KEY = 'api-key=dGpQ5OmGP2SgfvZimlpCUoF4iOag9q';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component{
+
+    state = {
+        reviews: [],
+        searchTerm: ''
+    }
+
+    inputChange = (event) => {
+        event.preventDefault()
+        fetch(URL)
+        .then(res => res.json())
+        .then(reviews => this.setState({reviews: reviews.results}))
+    }
+    render(){
+        return(
+            <div className="searchable-movie-reviews">
+                <form onSubmit={(event) => this.handleSearch(event)}>
+                <input onChange={(event) => this.handleChange(event)}/>
+                <button>Submit</button>
+            </form>
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
+
+
+export default SearchableMovieReviewsContainer
